b63e0dc7d7a68f1d1cb84ccef77d69919441a9a8
ChenQL6 <1571590774@qq.com>
2020-03-30 12:56:04 +0800

    20200330提交

---
+++ 3月10日实验2/3月10日实验2/3月10日实验2View.h
@@ -0,0 +1,51 @@
+
+// 3月10日实验2View.h : CMy3月10日实验2View 类的接口
+//
+
+#pragma once
+
+
+class CMy3月10日实验2View : public CView
+{
+protected: // 仅从序列化创建
+	CMy3月10日实验2View();
+	DECLARE_DYNCREATE(CMy3月10日实验2View)
+
+// 特性
+public:
+	CMy3月10日实验2Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMy3月10日实验2View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // 3月10日实验2View.cpp 中的调试版本
+inline CMy3月10日实验2Doc* CMy3月10日实验2View::GetDocument() const
+   { return reinterpret_cast<CMy3月10日实验2Doc*>(m_pDocument); }
+#endif
+
